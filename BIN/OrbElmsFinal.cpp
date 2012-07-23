e86867a69408f757a05b69d7b81924366f51a76a
curibe <uribeleon@gmail.com>
2012-07-22 23:39:10 -0500

    commit

--- BIN/OrbElmsFinal.cpp
+++ BIN/OrbElmsFinal.cpp
@@ -7,58 +7,75 @@ int main(int argc,char *argv[])
 {
 
   string Principal,r,cmd;
-  string Rundir,Bindir;
+  string Rundir,Bindir,pathAnal,Analysis;
   //string Rundir("/RUNDIR/");
   //string Bindir("/BIN/");
   string Rdir;
   string filePOS="BODY7.pos";
   string fileORB="BODY7.orb";
   string Fdump="Comet.pos";
+  string fileANAL="OrbElms.orb";
   string Nl,NameF,pathPOS,pathORB,Dir,out;
     
-  char *CMD;
-  int Ndir;
-  double *State;
-
+  char *CMD, *strl;
+  int Ndir,N;
+  double *State,output,*OrbElm;
+  
   FILE *POS,*ORB;
 
 
   //*******************
   // PATHS
   //*******************
-  Principal=exec("pwd");
-  Rundir=Principal+"/RUNDIR/";
-  Bindir=Principal+"/BIN";
+  Principal = exec("pwd");
+  Rundir = Principal+"/RUNDIR/";
+  Bindir = Principal+"/BIN";
+  Analysis = Principal + "/Analysis/";
+  pathAnal = Analysis + fileANAL;
 
   // Read the total number of runs
   cmd="ls -d "+Rundir+"* | wc -l";
   r=exec(cmd);
   Ndir=atoi(r.c_str());
-  double output;
-  char* strl;
-  int N;
+ 
+  
   for(int i=1;i<=Ndir;i++){
 
     
     //Path of folder output in each run directories
-    // Path of the Description file
     Dir=Rundir+"run"+toString(i)+"/output/";
+    
+    // Path of files BODY7.pos and BODY7.orb
     pathPOS = Dir + filePOS;
     pathORB = Dir + fileORB;
+    
     printf("Dir: %s , i: %d\n",Dir.c_str(),i);
+
+    // Reading the last line of BODY7.pos
     cmd = "tail -n 1 " + pathPOS;
     out = exec(cmd.c_str());
     printf("i:%d  out: %s\n",i,out.c_str());
     strl=(char *)out.c_str();
-    State=splitof(strl);
-    N=sizeof(State);
-    printf("N: %d\n",N);
-    for(int j=0;j<N;j++)
-      printf("%E\n",State[j]);
-    //printf("%lf %lf %lf %lf %lf %lf %lf %lf\n",State[0],State[1],State[2],State[3],State[4],State[5],State[6],State[7]);
+    State=SplitToF(strl);
+    //N=sizeof(State);
     
+    // Verifying if the comet has gotten out of the SOI
+    if(State[7]>RSOI){
+      printf("Run %d : has gotten out of SOI.\n");
+      cmd = "tail -n 1 " + pathORB;
+      out = exec(cmd.c_str());
+      OrbElm = SplitToF((char *)out.c_str());
+      ORB=fopen(pathAnal.c_str(),"a");
+      fprintf(ORB,"%4d   %10lf   %16E   %16E   %16E   %16E   %16E   %16E   %16E   %16E   %16E   %16E\n",
+	      i,OrbElm[0],OrbElm[1],OrbElm[2],OrbElm[3],OrbElm[4],OrbElm[5],OrbElm[6],OrbElm[7],OrbElm[8],OrbElm[9],
+	      OrbElm[10]);
+      fclose(ORB);     
+    }
+        
+    /*
+    for(int j=0;j<N;j++)
+    printf("%E\n",State[j]);*/
   }
-
-  
-
+ 
+  return 0;
 }
